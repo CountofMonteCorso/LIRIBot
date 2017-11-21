cfeb6eb42f0df6eb6b84649ba7f4001dca3513b8
Kevin <kcorso89@gmail.com>
2017-11-20 22:54:55 -0800

    post lord dave/ ~halfway pt

--- keys.js
+++ keys.js
@@ -1,10 +1,16 @@
-console.log('this is loaded');
-
-var twitterKeys = {
+var twitterKeysValue = {
   consumer_key: 'Wm2TiPNyAm1dJyjO4k4e0Z5mF',
   consumer_secret: 'CZUSoRGACLTSfgtpC1wIBWENiFcAhNoobijJCcWtvDKGNlkZlz',
   access_token_key: '931652877201784832-gDPTXaEH5VShW3qHFZFVWtrCDNQ6NeJ',
   access_token_secret: 'gapK7JYAragtwHMxbBtPWWNlVRXhHvosP6W4ufJw7wEKX',
 }
 
-module.exports = twitterKeys;
+
+var spotifyKeysValue = {
+	clientID: "ef657db1f6da4362a8006a20d3041bcf",
+	clientSecret: "529359f6ffcb49629348b3d191573d96",
+}
+module.exports = {
+	twitterKeys: twitterKeysValue,
+	spotifyKeys: spotifyKeysValue
+}
--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 var keys = require("./keys.js");
 var twitter = require("twitter");
-var twitterKeys = new twitter({
+var twitterClient = new twitter({
 	consumer_key: keys.twitterKeys.consumer_key,
 	consumer_secret: keys.twitterKeys.consumer_secret,
 	access_token_key: keys.twitterKeys.access_token_key,
 	access_token_secret: keys.twitterKeys.access_token_secret,
 })
-var params = {screen_name: 'nodejs'};
+var params = {screen_name: "corenforcer"};
 var userInput = process.argv;
 
 if (userInput[2] === "my-tweets") {
 	mytweets();
 } else if (userInput[2] === "spotify-this-song"){
-	spotifytThisSong();
+	spotifyThisSong();
 } else if (userInput[2] === "movie-this") {
 	movieThis();
 } else if (userInput[2] === "do-what-it-says") {
@@ -21,16 +21,47 @@ if (userInput[2] === "my-tweets") {
 
 
 function mytweets(){
+		twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+ 			 if (!error) {
+    				for (i = 0; i < 20 && i < tweets.length; i++){
+    					console.log(tweets[i].created_at);
+    					console.log(tweets[i].text);
+  					}
+  			}
+		}); 	
 
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
- 		 if (!error) {
-    		console.log(tweets);
-  		}
-	});
 };
 
-function spotifytThisSong(){
+function spotifyThisSong(){
+	var Spotify = require('node-spotify-api');
+	var params = {
+		type: 'track',
+		query: 'Shooting Stars'
+	}; 
+		if (userInput.length === 4) {
+			params.query = userInput[3];
+		}
+ 
+	var spotifyClient = new Spotify({
+  		id: keys.spotifyKeys.clientID,
+  		secret: keys.spotifyKeys.clientSecret,
+	});
+	spotifyClient.search(params, function(err, data) {
 
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+  			var track1 = data.tracks.items[0];
+  				console.log("Song title: " + track1.name); 
+				console.log("On the album " + track1.album.name);
+			//for loop for songs w/ multiple artists
+			for (var i = 0; i < track1.artists.length; i++) {
+				console.log("by " + track1.artists[i].name); 
+			}
+			//or needed for songs w/o previews
+				console.log("Preview song: " + (track1.preview_url || "No Preview Available...beyotch"))
+
+	});
 };
 
 function movieThis(){
